303e68c04a38d4eee58d5095bccaf9d1ad4f5251
ADMIN <suraj>
2015-06-12 12:40:16 +0530

    12June2015

---
+++ FXDesignUI/src/application/FXMLController.java
@@ -0,0 +1,57 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
+
+public class FXMLController implements Initializable{
+	
+	@FXML private Label actiontarget;
+	
+	@FXML private Canvas chartTop;
+	@FXML private Canvas chartBottom;
+    
+    @FXML 
+    protected void handleSubmitButtonAction(ActionEvent event) {
+
+        actiontarget.setText("Sign in button pressed");
+    }
+    
+    private void drawShapes(GraphicsContext gc) {
+        gc.setFill(Color.GREEN);
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(5);
+        gc.strokeLine(40, 10, 10, 40);
+        gc.fillOval(10, 60, 30, 30);
+        gc.strokeOval(60, 60, 30, 30);
+        gc.fillRoundRect(110, 60, 30, 30, 10, 10);
+        gc.strokeRoundRect(160, 60, 30, 30, 10, 10);
+        gc.fillArc(10, 110, 30, 30, 45, 240, ArcType.OPEN);
+        gc.fillArc(60, 110, 30, 30, 45, 240, ArcType.CHORD);
+        gc.fillArc(110, 110, 30, 30, 45, 240, ArcType.ROUND);
+        gc.strokeArc(10, 160, 30, 30, 45, 240, ArcType.OPEN);
+        gc.strokeArc(60, 160, 30, 30, 45, 240, ArcType.CHORD);
+        gc.strokeArc(110, 160, 30, 30, 45, 240, ArcType.ROUND);
+        gc.fillPolygon(new double[]{10, 40, 10, 40},
+                       new double[]{210, 210, 240, 240}, 4);
+        gc.strokePolygon(new double[]{60, 90, 60, 90},
+                         new double[]{210, 210, 240, 240}, 4);
+        gc.strokePolyline(new double[]{110, 140, 110, 140},
+                          new double[]{210, 210, 240, 240}, 4);
+    }
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+    	GraphicsContext gc = chartTop.getGraphicsContext2D();
+        drawShapes(gc);
+		
+	}
+}
